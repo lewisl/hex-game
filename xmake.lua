9c6b9d84b65ba2ab8b8d0723aeff868a294f90d4
Lewis Levin <lewis@neilson-levin.org>
2024-09-02 16:19:47 -0700

    compile as cxx17

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,8 @@
+add_rules("mode.release")
+
 target("hex") 
     set_kind("binary")
     add_files("src/hex.cpp", "src/game_play.cpp", "src/hex_board.cpp", "src/hex_board.cpp")
-    set_languages("cxx14")
+    set_languages("cxx17")
     set_optimize("fastest")
     add_cxxflags("-flto")  -- supposed to be linker optimization; doesn't really do much
